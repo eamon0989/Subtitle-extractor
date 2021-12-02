b9f2a9fb8eee3d1ad080982407858259d13e94b6
Eamon <eamon0989@gmail.com>
2021-12-02 15:15:50 +0100

    first commit

---
+++ subs.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+const data: string = fs.readFileSync('subs.srt', 'utf8');
+const lines: string[] = data.split('\r\n');
+const sentences: string[] = lines.filter(line => {
+  return /[a-zA-Z]/.test(line)
+})
+console.log(sentences)
